fffb6f24dee5753b4658d15d6df6c78927cdb405
chawlamit <amitchawla59@gmail.com>
2016-04-23 14:02:18 +0530

    created echo app

--- messenger.py
+++ messenger.py
@@ -3,6 +3,20 @@ import os
 import json
 import requests
 
+from flask_sqlalchemy import SQLAlchemy
+
+class token_reg(db.Model):
+    token = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -23,7 +37,8 @@ def verify_Webhook():
         print messaging_events
         for event in messaging_events:
             if 'message' in event and 'text' in event['message']:
-                print "....................:" + event['message']['text']
+                session['recipient'] = event['sender']['id']
+                reply(event['message']['text'])
                 break
         return "Done!"
 
@@ -38,11 +53,12 @@ def verify_Webhook():
 #Function that sends bot reply back to messenger
 def reply(msg):
     messageData = {"text":msg}
-    token = {"access_token":"CAAMpeMqMFaQBAD1rWOoDDLiGzn0wAlChwTnJy8fahWfUA8tx8OZAoeupSRlUBwZBkkRbsZC8UZCNiS1SEbhvKiLKdcZCInap0xBk79zJbAYZB0PRXRJTgBPigBiXH5OM7AfM6T91IjrnlBRnGxAJUyuqC3FHAL85wZAN98dZCf5u3UZABZAjSK2eolopQwZBoVw5j50dj1R9QlzKQZDZD"}
+    token = {"access_token":"CAADMARK7AawBAFZCiwNRiReYd94VDe1N1NrNdmGETGvd2fTZBmBbPjv6cLZBCo15ZA4FKuxdU2ydh6Ug3vqy9ig0rvQQaJc8BwJjZC0rlZA5lMCAxpmmscyNkclDQZAWulOXOqjfkGoo4XEGOCHNwjOLMqwxJ91ZCtr0OA61ZCAzv7m8J7t5GSfZCPZBjt75bNNyrruawv0KaSnrAZDZD"}
     content = {"recipient":{'id':session['recipient']}, "message":messageData}
     print content
     r =  requests.post('https://graph.facebook.com/v2.6/me/messages',params=token,json=content)
     print r.status_code
+
 
 port = int(os.getenv('VCAP_APP_PORT', 8080))
 if __name__ == '__main__':
